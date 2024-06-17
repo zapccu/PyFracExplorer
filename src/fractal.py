809d6e7075f4b64662c4c41fdc8933609025f6a9
zapccu <d.braner@gmx.net>
2024-06-17 21:31:22 +0200

    Initial commit

---
+++ src/fractal.py
@@ -0,0 +1,29 @@
+
+class Fractal:
+
+	name = ""
+
+	def __init__(self, name):
+		self.name = name
+
+class Mandelbrot(Fractal):
+
+	corner = complex(-1.5, -1.5)
+	size = complex(3.0, 3.0)
+	maxIter = 100
+	limit = 8.0
+
+	def __init__(self, corner: complex, size: complex, maxIter = 100, limit = 8.0):
+		self.corner = corner
+		self.size = size
+		self.maxIter = maxIter
+		self.limit = limit
+
+	def iterate(self, C: complex):
+		Z = C
+		i = 1
+
+		while i<self.maxIter and abs(Z) < self.limit:
+			Z = Z*Z+C
+			i += 1
+
