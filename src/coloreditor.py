00d34f57b6469878e09573b11f0426b7a6e1a830
zapccu <d.braner@gmx.net>
2024-07-23 17:39:09 +0200

    Minor changes

--- src/coloreditor.py
+++ src/coloreditor.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import colorchooser
 from tkinter.scrolledtext import ScrolledText
 
+
 class ColorEditor:
 
 	def __init__(self, mainWindow):
@@ -11,15 +12,14 @@ class ColorEditor:
 		self.dlg.title("Color Editor")
 
 		self.colorPoints = Frame(self.dlg, width=500, height=50)
-		self.colorPoints.pack_propagate(False)
-		self.colorPoints.pack(side=LEFT, expand=False, fill=RIGHT, anchor='nw')
+		self.colorPoints.place(x=10, y=20)
 
 		# Scrollbars
 		self.hScroll = Scrollbar(self.colorPoints, orient='horizontal')
 		self.hScroll.pack(fill=X, side=BOTTOM)
 
 		# Create drawing canvas and link with scrollbars
-		self.canvas = Canvas(self.colorPoints, width=600, height=50, bg='white', bd=0, highlightthickness=0, scrollregion=(0, 0, 600, 50))
+		self.canvas = Canvas(self.colorPoints, width=600, height=50, bg='black', bd=0, highlightthickness=0, scrollregion=(0, 0, 600, 50))
 		self.hScroll.config(command=self.canvas.xview)
 		self.canvas.config(xscrollcommand=self.hScroll.set)
 		self.canvas.pack(side=LEFT, expand=False, fill=NONE, anchor='nw')
